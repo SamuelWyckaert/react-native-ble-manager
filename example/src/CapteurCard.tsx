4d615d1d0f42832cdebd1373834bd990899763d4
Samuel <samwyckaert@hotmail.fr>
2023-07-28 10:49:07 +0200

    fix

--- example/src/CapteurCard.tsx
+++ example/src/CapteurCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { BleSingleton } from "./BleSingleton";
@@ -30,6 +30,40 @@ export const CapteurCard = (props: CapteurCardProps) => {
 
     const capteur = BleSingleton.get().capteur(name);
 
+    async function initSciChart() {
+      // LICENSING
+      // Commercial licenses set your license code here
+      // Purchased license keys can be viewed at https://www.scichart.com/profile
+      // How-to steps at https://www.scichart.com/licensing-scichart-js/
+      // SciChartSurface.setRuntimeLicenseKey("YOUR_RUNTIME_KEY");
+    
+      // Initialize SciChartSurface. Don't forget to await!
+      const { sciChartSurface, wasmContext } = await SciChartSurface.create("scichart-root", {
+        theme: new SciChartJsNavyTheme(),
+        title: "SciChart.js First Chart",
+        titleStyle: { fontSize: 22 }
+      });
+    
+      // Create an XAxis and YAxis with growBy padding
+      const growBy = new NumberRange(0.1, 0.1);
+      sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { axisTitle: "X Axis", growBy }));
+      sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { axisTitle: "Y Axis", growBy }));
+    
+      // Create a line series with some initial data
+      sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
+        stroke: "steelblue",
+        strokeThickness: 3,
+        dataSeries: new XyDataSeries(wasmContext, {
+          xValues: [0,1,2,3,4,5,6,7,8,9],
+          yValues: [0, 0.0998, 0.1986, 0.2955, 0.3894, 0.4794, 0.5646, 0.6442, 0.7173, 0.7833]
+        }),
+        pointMarker: new EllipsePointMarker(wasmContext, { width: 11, height: 11, fill: "#fff" }),
+        animation: new SweepAnimation({ duration: 300, fadeEffect: true })
+      }));
+    
+      return sciChartSurface;
+    }
+    
 
 
 
@@ -60,6 +94,7 @@ export const CapteurCard = (props: CapteurCardProps) => {
 
     ]
 
+    
 
     return (
         <View style={[styles.row, ]}>
@@ -73,9 +108,17 @@ export const CapteurCard = (props: CapteurCardProps) => {
           <Text> Nombre de trames par secondes :  { isCapteur && capteur && capteur.getTramesPerSecond()}</Text>
 
 
-            
-            <View id="scichart-root" style={{ maxWidth: 900 }} />
-                          
+          <Chart
+            style={{ height: 200, width: '100%', backgroundColor: '#eee' }}
+            xDomain={{ min: -2, max: 10 }}
+            yDomain={{ min: -2, max: 20 }}
+            padding={{ left: 20, top: 10, bottom: 10, right: 10 }}
+          >
+            <VerticalAxis tickValues={[0, 4, 8, 12, 16, 20]} />
+            <HorizontalAxis tickCount={3} />
+            <Line data={data[count % 2]} smoothing="none" theme={{ stroke: { color: 'red', width: 1 } }} />
+          </Chart>
+                    
 
 
         </View>
